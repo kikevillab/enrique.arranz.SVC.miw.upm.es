846d7ad5a939127750c3bfdf3d838a1b3dad430c
Enrique Arranz <enrique.arranz@urjc.es>
2016-09-28 18:26:13 +0200

    Creada la clase c51 y sus test

---
+++ src/test/java/enrique/arranz/SVC/miw/upm/es/C51Test.java
@@ -0,0 +1,26 @@
+package enrique.arranz.SVC.miw.upm.es;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C51Test {
+
+	C51 c;
+	
+	@Before
+	public void before(){
+		c = new C51();
+	}
+
+	@Test
+	public void testM1() {
+		assertEquals("m1", c.m1());
+	}
+
+	@Test
+	public void testM2() {
+		assertEquals("m2", c.m2());
+	}
+}
